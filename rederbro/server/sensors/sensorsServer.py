581a08782b729ce2ec9a2702ef8b2708780da06b
Simon Archieri <simon@archieri.fr>
2017-10-14 18:40:35 +0200

    a

--- rederbro/server/sensors/sensorsServer.py
+++ rederbro/server/sensors/sensorsServer.py
@@ -11,6 +11,39 @@ class SensorsServer(Server):
         self.distance = distance
         self.logger.info("Distance between photo set to {}".format(self.distance))
 
+    def toDegCord(self):#google earth and some other thing prefer degree coordinate
+        """
+        return latitude and longitude in degree
+        """
+        lat = self.lastCord[0]
+        lon = self.lastCord[1]
+        if lat !=None and lon != None and len(lat)>0 and len(lon)>0:#ensure that we have data to work with
+            try :
+                if lat[-1]=="N":#define the signe
+                    lat = float(lat[:-1])/100.0
+                else:
+                    lat = -float(lat[:-1])/100.0
+
+                if lon[-1]=="W":#define the signe
+                    lon = -float(lon[:-1])/100.0
+                else:
+                    lon = float(lon[:-1])/100.0
+            except (TypeError, ValueError):
+                return None, None
+            lat_deg = int(lat)
+            lat_min = (100.0*(float(lat)-lat_deg))/60
+            lat = lat_deg+lat_min
+
+            lon_deg = int(lon)
+            lon_min = (100.0*(float(lon)-lon_deg))/60
+            lon = lon_deg+lon_min
+
+            self.lastCord[0] = lat
+            self.lastCord[1] = lon
+            return self.lastCord
+
+        return 0, 0#mean it didin't work
+
     def getCoord(self):
         self.logger.info("Get coordonate")
         if self.fakeMode:
@@ -32,6 +65,8 @@ class SensorsServer(Server):
             self.lastCord = [(trame[2]+trame[3]), (trame[4]+trame[5]), trame[9]]
             self.lastTime = trame[1]
             self.lastHdop = trame[8]
+
+            self.toDegCord()
 
             self.logger.info("Coordonate : {}".format(self.lastCord))
             return self.lastCord
